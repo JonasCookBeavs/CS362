8f9764d61869f6fc1a18da2a75fcc2ec8af9d342
JonasCookBeavs <jonascook04@gmail.com>
2026-02-05 17:22:41 -0800

    EX5

---
+++ EX5/exercise05_observer.rb
@@ -0,0 +1,36 @@
+# Jonas Cook
+# Exercise 5: Observer Pattern
+
+class WeatherStation
+
+  attr_accessor :temperature
+
+  def initialize
+    @temperature = 0
+    @observers = []
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def temperature=(new_temperature)
+    @temperature = new_temperature
+    @observers.each do |o|
+      o.notify(self)
+    end
+  end
+
+end
+
+class StationObserver
+
+  def notify(station)
+    puts station.temperature
+  end
+
+end
+
+weather_station = WeatherStation.new
+weather_station.add_observer(StationObserver.new)
+weather_station.temperature = 72
